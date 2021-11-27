98dbd56ee9bf1e0de54bbd500f1a1d5dca9a1ca0
Rich Wareham <rich@richwareham.com>
2021-11-27 13:07:58 +0000

    macros and quiz4

--- exercises/quiz4.rs
+++ exercises/quiz4.rs
@@ -4,8 +4,11 @@
 // - Macros
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
-
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($v:expr) => {
+        format!("Hello {}", $v)
+    }
+}
 
 #[cfg(test)]
 mod tests {
